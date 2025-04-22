02f2b3f0a815a39d8019c6e2776cdbcee34a7de6
EdroCode <pedrocoutinho0606@gmail.com>
2025-04-22 21:29:05 +0100

    Initial code for gyroscope

--- Python/Sensores/BMP280.py
+++ Python/Sensores/BMP280.py
@@ -1,12 +1,10 @@
 import time
 import board
-
-# import digitalio # For use with SPI
 import adafruit_bmp280
+
 
 i2c = board.I2C()  
 bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
-
 bmp280.sea_level_pressure = 1013.25
 
 def get_bmp280_values():
