4937465bbd562f43c92ea2c2f44bb5e0782baf25
EdroCode <pedrocoutinho0606@gmail.com>
2025-04-22 21:50:46 +0100

    Gyroscope update

--- Python/Main.py
+++ Python/Main.py
@@ -5,27 +5,28 @@ from Sensores.BMP280 import get_bmp280_values
 from time import sleep
 from Sender import send_data, convert_data_to_json, verify_value
 from os import popen
-
+from Sensores.GY521 import MPU6050
 
 wait_time = 1
-
+global mpu
 
 def setup():
-    #mpu = MPU9250()
+    mpu = MPU6050()
     send_command_to_gps()
 
 
 def update(wait_time):
     
     inside_temp, inside_hum = get_dht22()
-    accel_values, gyro_values, mag_values = (0,0,0)  # mpu.get_all_sensor_data()
     pi_temp = popen("vcgencmd measure_temp").read().split('=')[1].split("'")[0]
     lat, lon, alt = get_gps_data()
     temp_bmp, pressure, alt_bmp = get_bmp280_values()
-
+    mpu_data = mpu.get_sensor_data()
+    accel_values = mpu_data['accel']
+    gyro_values = mpu_data['gyro']
 
     json_data = convert_data_to_json(inside_temp, inside_hum, accel_values, 
-                                     gyro_values, mag_values, pi_temp, lat, lon, alt, pressure,
+                                     gyro_values, pi_temp, lat, lon, alt, pressure,
                                      temp_bmp, alt_bmp)
     send_data(json_data)
     
--- Python/Sensores/GY521.py
+++ Python/Sensores/GY521.py
@@ -1,7 +1,7 @@
-import smbus					#import SMBus module of I2C
-from time import sleep          #import
+import smbus
+from time import sleep
 
-#some MPU6050 Registers and their Address
+# MPU6050 Registers and their Address
 PWR_MGMT_1   = 0x6B
 SMPLRT_DIV   = 0x19
 CONFIG       = 0x1A
@@ -14,65 +14,128 @@ GYRO_XOUT_H  = 0x43
 GYRO_YOUT_H  = 0x45
 GYRO_ZOUT_H  = 0x47
 
-
-def MPU_Init():
-	#write to sample rate register
-	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
-	
-	#Write to power management register
-	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
-	
-	#Write to Configuration register
-	bus.write_byte_data(Device_Address, CONFIG, 0)
-	
-	#Write to Gyro configuration register
-	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
-	
-	#Write to interrupt enable register
-	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
-
-def read_raw_data(addr):
-	#Accelero and Gyro value are 16-bit
-        high = bus.read_byte_data(Device_Address, addr)
-        low = bus.read_byte_data(Device_Address, addr+1)
-    
-        #concatenate higher and lower value
-        value = ((high << 8) | low)
+class MPU6050:
+    def __init__(self, bus_num=1, device_address=0x68):
+        self.bus = smbus.SMBus(bus_num)
+        self.device_address = device_address
+        self.accel_scale = 16384.0  # for +/- 2g range
+        self.gyro_scale = 131.0     # for +/- 250 deg/sec range
+        self.MPU_Init()
         
-        #to get signed value from mpu6050
-        if(value > 32768):
-                value = value - 65536
+    def MPU_Init(self):
+        """Initialize the MPU6050 sensor"""
+        # Write to sample rate register
+        self.bus.write_byte_data(self.device_address, SMPLRT_DIV, 7)
+        # Write to power management register
+        self.bus.write_byte_data(self.device_address, PWR_MGMT_1, 1)
+        # Write to Configuration register
+        self.bus.write_byte_data(self.device_address, CONFIG, 0)
+        # Write to Gyro configuration register
+        self.bus.write_byte_data(self.device_address, GYRO_CONFIG, 24)
+        # Write to interrupt enable register
+        self.bus.write_byte_data(self.device_address, INT_ENABLE, 1)
+    
+    def read_raw_data(self, addr):
+        """Read raw 16-bit value from the specified register"""
+        high = self.bus.read_byte_data(self.device_address, addr)
+        low = self.bus.read_byte_data(self.device_address, addr+1)
+        value = ((high << 8) | low)
+        if value > 32768:
+            value = value - 65536
         return value
+    
+    def get_sensor_data(self):
+        """Return all sensor data as a dictionary with calibrated values"""
+        # Read raw values
+        raw_acc_x = self.read_raw_data(ACCEL_XOUT_H)
+        raw_acc_y = self.read_raw_data(ACCEL_YOUT_H)
+        raw_acc_z = self.read_raw_data(ACCEL_ZOUT_H)
+        raw_gyro_x = self.read_raw_data(GYRO_XOUT_H)
+        raw_gyro_y = self.read_raw_data(GYRO_YOUT_H)
+        raw_gyro_z = self.read_raw_data(GYRO_ZOUT_H)
+        
+        # Convert to appropriate units
+        data = {
+            'accel': {
+                'x': raw_acc_x / self.accel_scale,
+                'y': raw_acc_y / self.accel_scale,
+                'z': raw_acc_z / self.accel_scale,
+                'units': 'g'
+            },
+            'gyro': {
+                'x': raw_gyro_x / self.gyro_scale,
+                'y': raw_gyro_y / self.gyro_scale,
+                'z': raw_gyro_z / self.gyro_scale,
+                'units': 'deg/s'
+            },
+            'raw': {
+                'accel': {'x': raw_acc_x, 'y': raw_acc_y, 'z': raw_acc_z},
+                'gyro': {'x': raw_gyro_x, 'y': raw_gyro_y, 'z': raw_gyro_z}
+            }
+        }
+        return data
 
+    def calibrate_sensor(self, samples=1000):
+        """
+        Calibrate the sensor by calculating offsets.
+        Place the sensor on a flat, level surface during calibration.
+        Returns offsets that can be applied to future readings.
+        """
+        print("Calibrating MPU6050... Keep sensor stationary on flat surface")
+        
+        # Variables to accumulate offsets
+        accel_x_offset = 0
+        accel_y_offset = 0
+        accel_z_offset = 0
+        gyro_x_offset = 0
+        gyro_y_offset = 0
+        gyro_z_offset = 0
+        
+        for _ in range(samples):
+            # Read raw values
+            accel_x = self.read_raw_data(ACCEL_XOUT_H)
+            accel_y = self.read_raw_data(ACCEL_YOUT_H)
+            accel_z = self.read_raw_data(ACCEL_ZOUT_H)
+            gyro_x = self.read_raw_data(GYRO_XOUT_H)
+            gyro_y = self.read_raw_data(GYRO_YOUT_H)
+            gyro_z = self.read_raw_data(GYRO_ZOUT_H)
+            
+            # Accumulate offsets
+            accel_x_offset += accel_x
+            accel_y_offset += accel_y
+            accel_z_offset += (accel_z - self.accel_scale)  # Expecting 1g in z-axis
+            gyro_x_offset += gyro_x
+            gyro_y_offset += gyro_y
+            gyro_z_offset += gyro_z
+            
+            sleep(0.001)
+        
+        # Calculate average offsets
+        offsets = {
+            'accel': {
+                'x': accel_x_offset / samples,
+                'y': accel_y_offset / samples,
+                'z': accel_z_offset / samples
+            },
+            'gyro': {
+                'x': gyro_x_offset / samples,
+                'y': gyro_y_offset / samples,
+                'z': gyro_z_offset / samples
+            }
+        }
+        
+        print("Calibration complete. Offsets:", offsets)
+        return offsets
 
-bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
-Device_Address = 0x68   # MPU6050 device address
-
-MPU_Init()
-
-print (" Reading Data of Gyroscope and Accelerometer")
-
-while True:
-	
-	#Read Accelerometer raw value
-	acc_x = read_raw_data(ACCEL_XOUT_H)
-	acc_y = read_raw_data(ACCEL_YOUT_H)
-	acc_z = read_raw_data(ACCEL_ZOUT_H)
-	
-	#Read Gyroscope raw value
-	gyro_x = read_raw_data(GYRO_XOUT_H)
-	gyro_y = read_raw_data(GYRO_YOUT_H)
-	gyro_z = read_raw_data(GYRO_ZOUT_H)
-	
-	#Full scale range +/- 250 degree/C as per sensitivity scale factor
-	Ax = acc_x/16384.0
-	Ay = acc_y/16384.0
-	Az = acc_z/16384.0
-	
-	Gx = gyro_x/131.0
-	Gy = gyro_y/131.0
-	Gz = gyro_z/131.0
-	
-
-	print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
-	sleep(1)
+# Example usage:
+if __name__ == "__main__":
+    mpu = MPU6050()
+    
+    # Calibrate the sensor (recommended on first use)
+    # offsets = mpu.calibrate_sensor()
+    # You would typically store these offsets and apply them to future readings
+    
+    # Get sensor data
+    data = mpu.get_sensor_data()
+    print("Acceleration (g):", data['accel'])
+    print("Rotation (deg/s):", data['gyro'])
