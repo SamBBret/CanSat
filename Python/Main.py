72b9a9b9317d63942b796814caf5e6d5d63ec123
Samuel Bret <samuelbret@icloud.com>
2026-04-24 16:41:57 +0100

    sab

--- Python/Main.py
+++ Python/Main.py
@@ -29,6 +29,8 @@ if __name__ == "__main__":
     print("Iniciando os Sensores...")
 
     try:
+        while True:
+            time.sleep(1)
     except KeyboardInterrupt:
         print("Programa terminado pelo utilizador.")
         if cam:                              
