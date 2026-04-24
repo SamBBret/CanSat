eabc30f2ee1f59c75d8bd8f05d062d1336482c66
Samuel Bret <samuelbret@icloud.com>
2026-04-24 16:22:23 +0100

    added LogData back in

---
+++ Python/LogData.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from time import sleep
+from Cam import CAMERA
+from os import popen
+import serial
+import subprocess
+
+wait_time = 5
+cam = None
+
+ser = None
+
+
+def setup():
+
+    timestamp = datetime.utcnow().isoformat()
+    global cam
+    cam = CAMERA(PATH)
+    cam.start_taking_photos_periodically(5)  # 15 segundos entre fotos!!
+
+if __name__ == "__main__":
+    setup()
+    print("Programa Inicializado")
+    sleep(5)
+    print("Iniciando os Sensores...")
+
+    try:
+        while True:
+            update(wait_time)
+    except KeyboardInterrupt:
+        print("Programa terminado pelo utilizador.")
+        if cam:                              
+            cam.stop_taking_photos()
